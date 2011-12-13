7ae83552337f2416656a5e87bf8dbd47ce45b181
kakadu.hafanana <kakadu.hafanana@gmail.com>
2011-12-13 18:54:19 +0300

    init commit

---
+++ Common/Attributes.fs
@@ -0,0 +1,40 @@
+﻿// Learn more about F# at http://fsharp.net
+module Parser.Common
+type parserStrategy = GLR | LALR | RecDes
+
+type ParserClassAttribute () =
+    inherit System.Attribute()
+
+type GLRAttribute () = 
+    inherit System.Attribute()
+
+type LALRAttribute () = 
+    inherit System.Attribute()
+
+type RecDesAttribute () = 
+    inherit System.Attribute()
+
+let isParserFunction (mem: System.Reflection.MemberInfo) = 
+  let attrs = mem.GetCustomAttributes false
+  attrs |> Array.fold (fun acc x -> 
+    match (acc,x) with
+    | (Some _,_) -> acc
+    | (_,(:? RecDesAttribute)) -> Some (RecDes)
+    | (_,(:? LALRAttribute)) -> Some (LALR)
+    | (_,(:? GLRAttribute)) -> Some (GLR)
+    | _ -> None
+    ) None
+
+let isParserClass (mem: System.Reflection.MemberInfo) = 
+  let attrs = mem.GetCustomAttributes false
+  mem.ReflectedType.IsClass &&
+    (attrs |> Array.exists (fun attr -> attr :? ParserClassAttribute))
+  
+let filter_map f xs = 
+  let acc = ref []
+  xs |> Array.iter (fun x ->
+    match f x with 
+    | Some y -> acc := y :: !acc
+    | None -> ()
+  )
+  acc
---
+++ Falka/Program.fs
@@ -0,0 +1,25 @@
+﻿open System.Reflection
+open Parser.Common
+
+let dll = Assembly.LoadFrom @"..\..\..\Test\bin\Debug\Test.dll"
+let types = dll.GetTypes ()
+let root = dll.GetType "Test"
+(* let () = Printf.printf "%s\n" (root.ToString ()) *)
+
+let parserClasses = Array.filter Parser.Common.isParserClass (root.GetMembers ())
+    
+let iterParserClass (c: MemberInfo) = 
+  assert (isParserClass c)
+  let f x = 
+    match isParserFunction x with
+    | Some a -> Some (a,x)
+    | None -> None
+  let members = c.ReflectedType.GetMembers  ()
+  let methods = Array.filter (fun (x: MemberInfo) -> x.MemberType = System.Reflection.MemberTypes.Method) members
+  let u = filter_map f methods
+  ()
+  
+
+let () = Array.iter iterParserClass parserClasses
+
+
